73b8ed4d868506abeafa2861f8a9a593240ae898
Kumar Hrithik <hrithikk2765@gmail.com>
2023-08-16 12:04:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+function App() {
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h2>Speech To Text Converter</h2>
+        <p>
+          A react hook that converts speech from the microphone and make sit
+          available to our React Components
+        </p>
+        <div className="main-content">
+          <p>{transcript}</p>
+        </div>
+        <div className="btn-style">
+          <button>Copy to clipboard</button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
